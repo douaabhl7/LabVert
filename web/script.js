7cfa1036a0ebc39f7ff2275fcb215526f400d88f
Douaaa <douaabhl7@gmail.com>
2026-02-02 18:39:20 -0500

    Add front-end
    Add event listener to refresh button to update temperature and humidity display.

---
+++ web/script.js
@@ -0,0 +1,5 @@
+document.getElementById("refresh").onclick = () => {
+  // données simulées pour l'instant
+  document.getElementById("temp").innerText = "Température : 23 °C";
+  document.getElementById("hum").innerText = "Humidité : 45 %";
+};
